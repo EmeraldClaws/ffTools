8f9e86230d37cd7617b311ead0fec8474df945ed
Alex Copland <alex@cop.land>
2023-11-01 20:50:05 +0000

    Add DTS to AudioCodecEnum

--- src/Enums/Codecs/AudioCodecsEnum.php
+++ src/Enums/Codecs/AudioCodecsEnum.php
@@ -8,12 +8,14 @@ enum AudioCodecsEnum: string implements StreamCodecInterface
 {
     case AAC = 'aac';
     case AC3 = 'ac3';
+    case DTS = 'dts';
 
     public function getLongName(): string
     {
         return match($this){
             self::AAC => "AAC (Advanced Audio Coding)",
             self::AC3 => "ATSC A/52A (AC-3)",
+            self::DTS => "DCA (DTS Coherent Acoustics)",
         };
     }
 
