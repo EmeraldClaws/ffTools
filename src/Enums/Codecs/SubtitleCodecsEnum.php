88a328e335c9348cfaaed60320dc9d27aa2cfc9b
Alex Copland <alex@cop.land>
2024-03-23 15:34:34 +0000

    Add dvd_subtitle and hdmv_pgs_subtitle to SubtitleCodecsEnum

--- src/Enums/Codecs/SubtitleCodecsEnum.php
+++ src/Enums/Codecs/SubtitleCodecsEnum.php
@@ -7,6 +7,8 @@ use Emeraldclaws\Fftools\Interfaces\StreamCodecInterface;
 enum SubtitleCodecsEnum: string implements StreamCodecInterface
 {
     case ASS = 'ass';
+    case DVD_SUBTITLE = 'dvd_subtitle';
+    case HDMV_PGS_SUBTITLE = 'hdmv_pgs_subtitle';
     case MOVTEXT = 'mov_text';
     case SUBRIP = 'subrip';
 
@@ -14,6 +16,8 @@ enum SubtitleCodecsEnum: string implements StreamCodecInterface
     {
         return match ($this) {
             self::ASS => 'ASS (Advanced SSA) subtitle',
+            self::DVD_SUBTITLE => 'DVD subtitles',
+            self::HDMV_PGS_SUBTITLE => 'HDMV Presentation Graphic Stream subtitles',
             self::MOVTEXT => 'MOV text',
             self::SUBRIP => 'SubRip subtitle',
         };
