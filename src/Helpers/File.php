d7759c2b58f16844ed156001b3bdb5dae679e36a
Alex Copland <alex@cop.land>
2024-04-15 21:25:10 +0100

    Allow files with ':' in name to be probed

--- src/Helpers/File.php
+++ src/Helpers/File.php
@@ -9,7 +9,7 @@ class File
 
     public static function probe(string $path)
     {
-        $ffprobeOutputRaw = Shell::shellExec("ffprobe -v quiet -print_format json -show_format -show_streams \"$path\" 2>&1");
+        $ffprobeOutputRaw = Shell::shellExec("ffprobe -v quiet -print_format json -show_format -show_streams file:\"$path\" 2>&1");
         $ffprobeOutput = json_decode($ffprobeOutputRaw, true);
 
         return new FfprobeData($ffprobeOutput['streams']);
