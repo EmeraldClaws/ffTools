59dc8848e8071b15a778e562ede82b097ce20b7e
Alex Copland <alex@cop.land>
2025-01-18 21:13:10 +0000

    Add additonal codecs

--- src/Enums/Codecs/VideoCodecsEnum.php
+++ src/Enums/Codecs/VideoCodecsEnum.php
@@ -6,10 +6,12 @@ use Emeraldclaws\Fftools\Interfaces\StreamCodecInterface;
 
 enum VideoCodecsEnum: string implements StreamCodecInterface
 {
+    case AV1 = 'av1';
     case FRAPS = 'fraps';
     case H264 = 'h264';
     case H265 = 'hevc';
     case MJPEG = 'mjpeg';
+    case MPEG1VIDEO = 'mpeg1video';
     case MPEG4 = 'mpeg4';
     case VP9 = 'vp9';
 
@@ -17,10 +19,12 @@ enum VideoCodecsEnum: string implements StreamCodecInterface
     public function getLongName(): string
     {
         return match ($this) {
-            self::FRAPS => 'Fraps',
+            self::AV1 => "Alliance for Open Media AV1",
+            self::FRAPS => "Fraps",
             self::H264 => "H.264 / AVC / MPEG-4 AVC / MPEG-4 part 10",
             self::H265 => "H.265 / HEVC (High Efficiency Video Coding)",
             self::MJPEG => "Motion JPEG",
+            self::MPEG1VIDEO => "MPEG-1 video",
             self::MPEG4 => "MPEG-4 part 2",
             self::VP9 => "Google VP9",
         };
